dae90f38cd6fbc6562deb77f7f0d1425bc1d1abe
Kelsey McArthur <kelseymcarthur@kelseys-macbook-pro.local>
2019-01-15 18:38:50 -0500

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Kelsey McArthur on 1/15/19.
+//  Copyright © 2019 Kelsey McArthur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
